b240c54e3f0287e3586fdd2139be0b288f8a656c
Giulia Alfonsi <giulia@ocastastudios.com>
2015-09-07 15:56:01 +0100

    fixed static server for testing ui

--- public/server.js
+++ public/server.js
@@ -1,35 +1,8 @@
 var express = require('express');
-var multer = require('multer');
 var app = express();
-
-var done = false;
-
-/*Configure the multer.*/
-app.use(multer({ dest: './comic/images',
-  limits: {
-    fieldSize: 100000000
-  },
-  rename: function (fieldname, filename) {
-    return filename;
-  },
-  onFileUploadStart: function (file) {
-    // console.log(file.originalname + ' is starting ...');
-  },
-  onFileUploadComplete: function (file) {
-    // console.log(file.fieldname + ' uploaded to  ' + file.path);
-    done = true;
-  }
-}));
 
 /*Handling routes.*/
 app.use(express.static(__dirname + '/'));
-
-app.post('/upload',function(req, res){
-  if(done === true){
-    // console.log(req.files);
-    res.end('{"status": "ok", "form": ' + JSON.stringify(req.files) + '}');
-  }
-});
 
 /*Run the server.*/
 app.listen(process.env.PORT || 8000);
